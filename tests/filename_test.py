9ca8700e6cdc92720f92f6d146853eb13417ea99
brworkit <53059568+brworkit@users.noreply.github.com>
2020-10-02 10:18:56 -0300

    Initialize dev

---
+++ tests/filename_test.py
@@ -0,0 +1,13 @@
+from pkgname import filename
+
+# TODO:
+    # Human-intelligible date
+    # https://strftime.org/  apply theses rules
+
+def call():
+    return True
+
+def test_sample():
+    result = filename.call()
+    assert result == True
+
