afcc45a1424eb2ebb2ebff25d7ae23bdc9a797d0
Bruno Barbosa <brworkit@gmail.com>
2020-10-15 13:12:07 -0300

    version .6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hokage",
-    version="0.0.5",
+    version="0.0.6",
     author="brworkit",
     author_email="brworkit@gmail.com",
     description="A python package for avoid repetitions.",
