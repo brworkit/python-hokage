b3ddbcd40cebd95fbdc8d4e94366b3ca1f3722d0
Bruno Barbosa <brworkit@gmail.com>
2020-10-15 13:11:25 -0300

    print bucket creating

--- hokage/s3.py
+++ hokage/s3.py
@@ -1,12 +1,27 @@
 import boto3
 
-def create_bucket_with_options(bucket_name, tag_set, server_side_encryption_rules, acl='private', region='us-east-1'):
+def create_bucket_with_options(bucket_name,
+                               tag_set,
+                               server_side_encryption_rules,
+                               acl='private',
+                               region='us-east-1',
+                               enable_versioning=False):
+
+    print("create_bucket_with_options")
+    print(f"bucket_name: {bucket_name}")
+    print(f"region: {region}")
+    print(f"enable_versioning: {enable_versioning}")
+
     s3_resource = boto3.resource('s3', region_name=region)
 
     response_bucket_create = s3_resource.create_bucket(
         ACL=acl,
         Bucket=bucket_name
     )
+
+    if enable_versioning:
+        bkt_versioning = s3_resource.BucketVersioning(bucket_name)
+        bkt_versioning.enable()
 
     s3_client = boto3.client('s3', region_name=region)
 
